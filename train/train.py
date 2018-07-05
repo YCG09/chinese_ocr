abd03ca712eec06a192eba63653e8717a4fbacdd
YCG09 <18601969997@126.com>
2018-07-05 18:49:52 +0800

    Fix bug of python2

--- train/train.py
+++ train/train.py
@@ -21,7 +21,7 @@ from keras.optimizers import SGD, Adam
 from keras.models import Model
 from keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler, TensorBoard
 
-from imp import reload 
+from imp import reload
 import densenet
 
 
@@ -30,21 +30,21 @@ img_w = 280
 batch_size = 128
 maxlabellength = 10
 
-def get_session(gpu_fraction=1.0):  
-  
+def get_session(gpu_fraction=1.0):
+
     num_threads = os.environ.get('OMP_NUM_THREADS')
-    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)  
-  
-    if num_threads:  
-        return tf.Session(config=tf.ConfigProto(  
-            gpu_options=gpu_options, intra_op_parallelism_threads=num_threads))  
-    else:  
-        return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))  
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
+
+    if num_threads:
+        return tf.Session(config=tf.ConfigProto(
+            gpu_options=gpu_options, intra_op_parallelism_threads=num_threads))
+    else:
+        return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 def readfile(filename):
     res = []
     with open(filename, 'r') as f:
-        lines = f.readlines() 
+        lines = f.readlines()
         for i in lines:
             res.append(i.strip())
     dic = {}
@@ -74,8 +74,8 @@ class random_uniform_num():
         else:
             r_n = self.range[self.index : self.index + batchsize]
             self.index = self.index + batchsize
-        
-        return r_n  
+
+        return r_n
 
 def gen(data_file, image_path, batchsize=128, maxlabellength=10, imagesize=(32, 280)):
     image_label = readfile(data_file)
@@ -84,7 +84,7 @@ def gen(data_file, image_path, batchsize=128, maxlabellength=10, imagesize=(32, 
     labels = np.ones([batchsize, maxlabellength]) * 10000
     input_length = np.zeros([batchsize, 1])
     label_length = np.zeros([batchsize, 1])
-    
+
     r_n = random_uniform_num(len(_imagefile))
     _imagefile = np.array(_imagefile)
     while 1:
@@ -93,22 +93,22 @@ def gen(data_file, image_path, batchsize=128, maxlabellength=10, imagesize=(32, 
             img1 = Image.open(os.path.join(image_path, j)).convert('L')
             img = np.array(img1, 'f') / 255.0 - 0.5
 
-            x[i] = np.expand_dims(img,axis=2)
+            x[i] = np.expand_dims(img, axis=2)
             # print('imag:shape', img.shape)
             str = image_label[j]
-            label_length[i] = len(str) 
-            
+            label_length[i] = len(str)
+
             if(len(str) <= 0):
                 print("len < 0", j)
             input_length[i] = imagesize[1] // 8
-            labels[i, :len(str)] =[int(i) - 1 for i in str]
+            labels[i, :len(str)] = [int(k) - 1 for k in str]
 
         inputs = {'the_input': x,
                 'the_labels': labels,
                 'input_length': input_length,
                 'label_length': label_length,
                 }
-        outputs = {'ctc': np.zeros([batchsize])} 
+        outputs = {'ctc': np.zeros([batchsize])}
         yield (inputs, outputs)
 
 def ctc_lambda_func(args):
@@ -126,7 +126,7 @@ def get_model(img_h, nclass):
     input_length = Input(name='input_length', shape=[1], dtype='int64')
     label_length = Input(name='label_length', shape=[1], dtype='int64')
 
-    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length]) 
+    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
 
     model = Model(inputs=[input, labels, input_length, label_length], outputs=loss_out)
     model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer='adam', metrics=['accuracy'])
@@ -138,7 +138,7 @@ if __name__ == '__main__':
     char_set = open('char_std_5990.txt', 'r', encoding='utf-8').readlines()
     char_set = ''.join([ch.strip('\n') for ch in char_set][1:] + ['卍'])
     nclass = len(char_set)
-    
+
     K.set_session(get_session())
     reload(densenet)
     basemodel, model = get_model(img_h, nclass)
@@ -160,11 +160,11 @@ if __name__ == '__main__':
     tensorboard = TensorBoard(log_dir='./models/logs', write_graph=True)
 
     print('-----------Start training-----------')
-    model.fit_generator(train_loader, 
-    	steps_per_epoch = 3607567 // batch_size, 
-    	epochs = 10, 
-    	initial_epoch = 0, 
-    	validation_data = test_loader, 
-    	validation_steps = 36440 // batch_size, 
+    model.fit_generator(train_loader,
+    	steps_per_epoch = 3607567 // batch_size,
+    	epochs = 10,
+    	initial_epoch = 0,
+    	validation_data = test_loader,
+    	validation_steps = 36440 // batch_size,
     	callbacks = [checkpoint, earlystop, changelr, tensorboard])
 
